a2d9baa7690954f62fa5c6753f7f682ef8f0601c
Andreas Sivertse <201611235@post.au.dk>
2018-09-19 10:39:13 +0200

    200 test potions

--- test_rle.py
+++ test_rle.py
@@ -1,6 +1,21 @@
 from rle import rle_encoder
+from rle import rle_decoder
+from hypothesis import given, settings
+from hypothesis.strategies import text
 
 def test_simple():
     assert rle_encoder("bbbkkk") == "b3k3"
 def test_advanced():
     assert rle_encoder("ffffiiiii") == "f4i5"
+def test_advanced1():
+    assert rle_encoder("ffffggggbbbb444") == "f4g4b443"
+
+def test_simple():
+    assert rle_decoder("f3g6") == "fffgggggg"
+
+
+@given(text())
+@settings (max_examples=200)
+def test_hypo(x):
+    print x
+    assert rle_decoder(rle_encoder(x)) == x
